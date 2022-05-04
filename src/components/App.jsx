618478c4853bf0a9b90314b7b1ae3b0a7632b9cd
Anton K <89594975+Pusttak@users.noreply.github.com>
2022-05-04 18:06:38 +0400

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,16 +18,20 @@ export class App extends Component {
     });
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const buttonsName = Object.keys(this.state);
-
-    const countTotalFeedback = Object.values(this.state).reduce(
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce(
       (acc, feedback) => acc + feedback,
       0
     );
-    const countPositiveFeedbackPercentage =
-      Math.ceil((100 / countTotalFeedback) * good) || 0;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.ceil((100 / this.countTotalFeedback()) * this.state.good) || 0;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const buttonsName = Object.keys(this.state);
 
     return (
       <div
@@ -51,10 +55,11 @@ export class App extends Component {
             good={good}
             neutral={neutral}
             bad={bad}
-            total={countTotalFeedback}
-            positivePercentage={countPositiveFeedbackPercentage}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
           />
         </Section>
+        this.countTotalFeedback()
       </div>
     );
   }
